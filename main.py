0e87168f2d4551e2f43e2573183da2782e78230b
AvGust-Suns <78925270+AvGust-Suns@users.noreply.github.com>
2021-02-15 18:17:43 +0300

    Версия 0.0.1: *добавлен курсор *добавлено название игры

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello Anatoliy Nikolaev")
+import pygame
+
+
+def draw_cursor(coord, cursor):
+    pygame.mouse.set_visible(False)
+    screen.fill(pygame.Color('black'))
+    screen.blit(cursor, coord)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption("Plants vs monsters")
+    size = width, height = 1500, 1000
+    screen = pygame.display.set_mode(size)
+    cursor = pygame.image.load("cursor.png")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEMOTION:
+                if pygame.mouse.get_focused():
+                    draw_cursor(pygame.mouse.get_pos(), cursor)
+                else:
+                    screen.fill(pygame.Color('black'))
+                pygame.display.flip()
+
+    pygame.quit()
