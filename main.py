0ac82b7b21eaa3eafb6d0812f5cb9513392c4ad4
AvGust-Suns <78925270+AvGust-Suns@users.noreply.github.com>
2021-02-16 10:25:15 +0300

    Версия 0.1.0: *добавлены растения *можно выбрать растения *пофикшен баг с упадком fps при переходе к основной игре

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def open_windows():
         play_window()
 
 
-def draw_cursor(coord, cursor):
-    pygame.mouse.set_visible(False)
+def draw_cursor(coord):
     screen.blit(cursor, coord)
 
 
@@ -30,6 +29,16 @@ def help_window():
 
 def play_window():
     screen.blit(play_background, (0, 0))
+    for i in range(8):
+        screen.blit(page, (i * 152, 0))
+    screen.blit(tomato, (20, 20))
+    screen.blit(sunflower, (175, 20))
+    screen.blit(pee, (335, 20))
+    screen.blit(nut, (485, 20))
+    screen.blit(double_pee, (625, 20))
+    screen.blit(cherry, (780, 20))
+    screen.blit(thorns, (930, 20))
+    screen.blit(shovel, (1080, 20))
 
 
 def button_check_start(coord):
@@ -38,15 +47,18 @@ def button_check_start(coord):
     x, y = coord[0], coord[1]
     if 940 >= x >= 590 and 480 >= y >= 320:
         fl = "play"
+        open_windows()
+        screen.blit(cursor, coord)
+        pygame.display.flip()
         pygame.mixer.music.load('play_music.mp3')
         pygame.mixer.music.play()
     if 1000 >= x >= 550 and 720 >= y >= 640:
         fl = "help"
+        open_windows()
+        screen.blit(cursor, coord)
+        pygame.display.flip()
     if 950 >= x >= 580 and 850 >= y >= 770:
         running = False
-    open_windows()
-    screen.blit(cursor, coord)
-    pygame.display.flip()
 
 
 def button_check_help(coord):
@@ -54,9 +66,34 @@ def button_check_help(coord):
     x, y = coord[0], coord[1]
     if 1400 >= x >= 1100 and 700 >= y >= 250:
         fl = "start"
-    open_windows()
-    screen.blit(cursor, coord)
-    pygame.display.flip()
+        open_windows()
+        screen.blit(cursor, coord)
+        pygame.display.flip()
+
+
+def button_check_play(coord):
+    global fl_cursor
+    global cursor
+    x, y = coord[0], coord[1]
+    if 155 >= y >= 0:
+        if 155 >= x >= 1:
+            cursor = tomato
+        elif 256 >= x >= 156:
+            cursor = sunflower
+        elif 406 >= x >= 306:
+            cursor = pee
+        elif 556 >= x >= 456:
+            cursor = nut
+        elif 706 >= x >= 606:
+            cursor = double_pee
+        elif 856 >= x >= 756:
+            cursor = cherry
+        elif 1006 >= x >= 906:
+            cursor = thorns
+        elif 1156 >= x >= 1056:
+            cursor = shovel
+        if 1156 >= x >= 1:
+            fl_cursor = "not const"
 
 
 if __name__ == '__main__':
@@ -64,19 +101,33 @@ if __name__ == '__main__':
     pygame.display.set_caption("Plants vs monsters")
     size = width, height = 1500, 1000
     screen = pygame.display.set_mode(size)
+    pygame.mouse.set_visible(False)
+    clock = pygame.time.Clock()
     cursor = pygame.image.load("cursor.png")
-    background = pygame.image.load("background.png")
-    help_background = pygame.image.load("help_background.png")
-    play_background = pygame.image.load("game_background.png")
-    Bplay = pygame.image.load("button_play.png")
-    Hplay = pygame.image.load("button_help.png")
-    Eplay = pygame.image.load("button_exit.png")
-    H_back = pygame.image.load("back_help.png")
+    fl_cursor = "const"
+    background = pygame.image.load("background.png").convert()
+    help_background = pygame.image.load("help_background.png").convert()
+    play_background = pygame.image.load("game_background.png").convert()
+    Bplay = pygame.image.load("button_play.png").convert()
+    Hplay = pygame.image.load("button_help.png").convert()
+    Eplay = pygame.image.load("button_exit.png").convert()
+    H_back = pygame.image.load("back_help.png").convert()
+    page = pygame.image.load("page.png").convert()
+    tomato = pygame.image.load("tomato.png")
+    sunflower = pygame.image.load("sunflower.png")
+    nut = pygame.image.load("nut.png")
+    pee = pygame.image.load("pee.png")
+    double_pee = pygame.image.load("double_pee.png")
+    cherry = pygame.image.load("cherry.png")
+    thorns = pygame.image.load("thorns.png")
+    shovel = pygame.image.load("shovel.png")
     fl = "start"
     pygame.mixer.music.load('music_start.mp3')
     pygame.mixer.music.play()
     running = True
     while running:
+        open_windows()
+        draw_cursor(pygame.mouse.get_pos())
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -86,12 +137,13 @@ if __name__ == '__main__':
                 if fl == "help":
                     button_check_help(pygame.mouse.get_pos())
                 if fl == "play":
-                    pass
+                    button_check_play(pygame.mouse.get_pos())
             if event.type == pygame.MOUSEMOTION:
                 open_windows()
                 if pygame.mouse.get_focused():
-                    draw_cursor(pygame.mouse.get_pos(), cursor)
-                else:
-                    pass
+                    draw_cursor(pygame.mouse.get_pos())
                 pygame.display.flip()
-    pygame.quit()
+            clock.tick(60)
+            pygame.display.set_caption("fps: " + str(clock.get_fps()))
+            pygame.display.update()
+pygame.quit()
