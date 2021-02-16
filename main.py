69b1e6c43438d976e36c8efbd515c5d1fee7366a
AvGust-Suns <78925270+AvGust-Suns@users.noreply.github.com>
2021-02-16 16:03:30 +0300

    Версия 0.1.1:

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import datetime
+import random
 
 
 def open_windows():
@@ -74,6 +76,7 @@ def button_check_help(coord):
 def button_check_play(coord):
     global fl_cursor
     global cursor
+    global mas_flowers
     x, y = coord[0], coord[1]
     if 155 >= y >= 0:
         if 155 >= x >= 1:
@@ -94,19 +97,31 @@ def button_check_play(coord):
             cursor = shovel
         if 1156 >= x >= 1:
             fl_cursor = "not const"
-    if fl_cursor == "not const":
+    if fl_cursor == "not const" and cursor_const != cursor:
         if 930 >= y >= 155 and 1550 >= x >= 1:
-            x = x // 155 * 155
-            y = (y - 155) // 155 * 155
-        if x % 155 == 0 and y % 155 == 0:
-            #"""""""""""""""""""mas_flowers[x][y] = cursor
+            x = x // 155
+            y = (y - 155) // 155
+            if mas_flowers[x][y] == nots and cursor != shovel:
+                mas_flowers[x][y] = cursor
+            elif cursor == shovel:
+                mas_flowers[x][y] = nots
             cursor = cursor_const
-    print(x, y)
+            fl_cursor = "const"
+
+
+def draw_plants():
+    for i in range(9):
+        for j in range(5):
+            screen.blit(mas_flowers[i][j], (i * 155 + 20, j * 155 + 175))
+
+
+def sun_down():
+    screen.blit(sun, (1400, 50))
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption("Plants vs monsters")
+    pygame.display.set_caption("Plants vs zombos")
     size = width, height = 1500, 1000
     screen = pygame.display.set_mode(size)
     pygame.mouse.set_visible(False)
@@ -114,11 +129,10 @@ if __name__ == '__main__':
     cursor_const = cursor = pygame.image.load("cursor.png")
     fl_cursor = "const"
     nots = pygame.image.load("not.png")
-    mas_flowers = []
+    mas_flowers = [[nots] * 10 for i in range(10)]
     for i in range(9):
-        mas_flowers.append([])
         for j in range(5):
-            mas_flowers[i].append(nots)
+            mas_flowers[i][j] = nots
     print(mas_flowers)
     background = pygame.image.load("background.png").convert()
     help_background = pygame.image.load("help_background.png").convert()
@@ -136,14 +150,20 @@ if __name__ == '__main__':
     cherry = pygame.image.load("cherry.png")
     thorns = pygame.image.load("thorns.png")
     shovel = pygame.image.load("shovel.png")
+    sun = pygame.image.load("sun.png")
     fl = "start"
     # pygame.mixer.music.load('music_start.mp3')
-    #pygame.mixer.music.play()
+    # pygame.mixer.music.play()
     running = True
+    open_windows()
+    pygame.display.flip()
+    time_fl = 0
+    now = datetime.datetime.now()
+    then = datetime.datetime.now()
     while running:
-        open_windows()
-        draw_cursor(pygame.mouse.get_pos())
         for event in pygame.event.get():
+            open_windows()
+            draw_plants()
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -154,10 +174,9 @@ if __name__ == '__main__':
                 if fl == "play":
                     button_check_play(pygame.mouse.get_pos())
             if event.type == pygame.MOUSEMOTION:
-                open_windows()
-                if pygame.mouse.get_focused():
-                    draw_cursor(pygame.mouse.get_pos())
-                pygame.display.flip()
+                draw_cursor(pygame.mouse.get_pos())
+            draw_cursor(pygame.mouse.get_pos())
+            pygame.display.flip()
             clock.tick(60)
             pygame.display.set_caption("fps: " + str(clock.get_fps()))
             pygame.display.update()
