1ff390882b6f59614355eb4c8253c7f4fe15e687
AvGust-Suns <78925270+AvGust-Suns@users.noreply.github.com>
2021-02-22 19:19:54 +0300

    Версия 2.0.0: *добавлен зомби *добавлена механика поедания *добавлена механика съедания растений *добавлены анимации для зомби *смена курсора *смена колючек *анимации для ореха

--- main.py
+++ main.py
@@ -139,10 +139,14 @@ def button_check_play(coord):
             if mas_flowers[x][y] == nots and cursor != shovel:
                 score -= cost[cursor]
                 mas_flowers[x][y] = cursor
+                hp_flowers[x][y] = 50
+                if cursor == nut:
+                    hp_flowers[x][y] = 500
                 if cursor == sunflower:
                     sunflower_times[x][y] = time.time()
             elif cursor == shovel:
                 mas_flowers[x][y] = nots
+                hp_flowers[x][y] = 0
             cursor = cursor_const
             fl_cursor = "const"
 
@@ -188,6 +192,89 @@ def draw_text():
     screen.blit(text_surface, text_rect)
 
 
+def draw_zombies():
+    global zombie_anim_fl
+    global zombies_anim
+    global zombies_x
+    global zombies_y
+    global zombie_eat_fl
+    global mas_flowers
+    if fl == "play":
+        if len(zombies_x) == 0:
+            zombies_x.append(1500)
+            zombies_y.append(zombies_y_const[random.randrange(5)])
+            zombie_eat_fl.append(0)
+            zombie_anim_fl.append(0)
+            zombies_anim.append(0)
+        for i in range(len(zombies_x)):
+            if zombie_eat_fl[i] == 0:
+                zombies_x[i] -= 0.5
+                screen.blit(anim_number[zombies_anim[i]], (zombies_x[i], zombies_y[i]))
+                x1 = zombies_x[i] // 150
+                y1 = (zombies_y[i]) // 150
+                for y in range(5):
+                    for x in range(9):
+                        if mas_flowers[x][y] != nots:
+                            if x1 == x and y1 == y:
+                                zombie_eat_fl[i] = 1
+                if zombie_anim_fl[i] < 9:
+                    zombie_anim_fl[i] += 1
+                elif zombie_anim_fl[i] == 9:
+                    zombie_anim_fl[i] = 0
+                    zombies_anim[i] += 1
+                    zombies_anim[i] %= 3
+            else:
+                dop_trash = zombies_anim[i]
+                if dop_trash == 1 or dop_trash == 2:
+                    zombies_anim[i] = 0
+                    zombie_anim_fl[i] = 0
+                if zombie_anim_fl[i] < 20:
+                    zombie_anim_fl[i] += 1
+                elif zombie_anim_fl[i] == 20:
+                    zombie_anim_fl[i] = 0
+                    zombies_anim[i] = 3 - zombies_anim[i]
+                    x1 = zombies_x[i] // 150
+                    y1 = (zombies_y[i]) // 150
+                    for y in range(5):
+                        for x in range(9):
+                            if x1 == x and y1 == y:
+                                hp_flowers[x][y] -= 10
+                                if hp_flowers[x][y] == 300:
+                                    mas_flowers[x][y] = nut_1
+                                if hp_flowers[x][y] == 100:
+                                    mas_flowers[x][y] = nut_2
+                                if hp_flowers[x][y] == 0:
+                                    mas_flowers[x][y] = nots
+                                    zombie_eat_fl[i] = 0
+                screen.blit(anim_number[zombies_anim[i]], (zombies_x[i], zombies_y[i]))
+
+
+def draw_sun():
+    global prev_time
+    global flag_sun_1
+    global flag_sun_2
+    global flag_sun_3
+    if fl == "play" and time.time() - prev_time > 20:
+        prev_time = time.time()
+        if flag_sun_1 == 0:
+            flag_sun_1 = 1
+        elif flag_sun_2 == 0:
+            flag_sun_2 = 1
+        else:
+            flag_sun_3 = 1
+    if flag_sun_1:
+        sun_down_1()
+    if flag_sun_2:
+        sun_down_2()
+    if flag_sun_3:
+        sun_down_3()
+    for x in range(5):
+        for y in range(9):
+            if flag_sunflowers[x][y] == 1:
+                screen.blit(sun, square_centres[x][y])
+                sunflower_times[x][y] = time.time()
+
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption("Plants vs zombos")
@@ -200,10 +287,24 @@ if __name__ == '__main__':
     fl_cursor = "const"
     nots = pygame.image.load("not.png")
     mas_flowers = [[nots] * 10 for i in range(10)]
+    hp_flowers = [[0] * 10 for i in range(10)]
     for i in range(9):
         for j in range(5):
             mas_flowers[i][j] = nots
-    print(mas_flowers)
+            hp_flowers[i][j] = 0
+    zombies_x = []
+    zombies_y = []
+    zombies_y_const = [100, 250, 400, 550, 700]
+    zombies_anim = []
+    zombie_anim_1 = pygame.image.load("zombie_const.png")
+    zombie_anim_2 = pygame.image.load("zombie_rightleg.png")
+    zombie_anim_3 = pygame.image.load("zombie_leftleg.png")
+    zombie_anim_4 = pygame.image.load("zombie_eat.png")
+    anim_number = [zombie_anim_1, zombie_anim_2, zombie_anim_3, zombie_anim_4]
+    zombie_eat_fl = []
+    zombie_anim_fl = []
+    zombie_helmet =[]
+    zombie_hp = []
     score = 100
     font_name = pygame.font.match_font('arial')
     font = pygame.font.Font(font_name, 50)
@@ -218,15 +319,19 @@ if __name__ == '__main__':
     tomato = pygame.image.load("tomato.png")
     sunflower = pygame.image.load("sunflower.png")
     nut = pygame.image.load("nut.png")
+    nut_1 = pygame.image.load("nut_2.png")
+    nut_2 = pygame.image.load("nut_3.png")
     pee = pygame.image.load("pee.png")
     double_pee = pygame.image.load("double_pee.png")
     cherry = pygame.image.load("cherry.png")
     thorns = pygame.image.load("thorns.png")
     shovel = pygame.image.load("shovel.png")
     sun = pygame.image.load("sun.png")
+    pos_mouse_x = -100
+    pos_mouse_y = -100
     fl = "start"
-    pygame.mixer.music.load('music_start.mp3')
-    pygame.mixer.music.play()
+    # pygame.mixer.music.load('music_start.mp3')
+    # pygame.mixer.music.play()
     running = True
     open_windows()
     clock_time.tick(60)
@@ -244,10 +349,10 @@ if __name__ == '__main__':
     flag_sun_1 = 0
     flag_sun_2 = 0
     flag_sun_3 = 0
+    mouse_x = -100
+    mouse_y = -100
     while running:
         for event in pygame.event.get():
-            open_windows()
-            draw_plants()
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -257,8 +362,6 @@ if __name__ == '__main__':
                     button_check_help(pygame.mouse.get_pos())
                 if fl == "play":
                     button_check_play(pygame.mouse.get_pos())
-            if event.type == pygame.MOUSEMOTION:
-                draw_cursor(pygame.mouse.get_pos())
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     if volume > 0:
@@ -268,27 +371,14 @@ if __name__ == '__main__':
                     if volume < 1:
                         volume += 0.1
                     pygame.mixer.music.set_volume(volume)
-            if fl == "play" and time.time() - prev_time > 20:
-                prev_time = time.time()
-                if flag_sun_1 == 0:
-                    flag_sun_1 = 1
-                elif flag_sun_2 == 0:
-                    flag_sun_2 = 1
-                else:
-                    flag_sun_3 = 1
-            if flag_sun_1:
-                sun_down_1()
-            if flag_sun_2:
-                sun_down_2()
-            if flag_sun_3:
-                sun_down_3()
-            for x in range(5):
-                for y in range(9):
-                    if flag_sunflowers[x][y] == 1:
-                        screen.blit(sun, square_centres[x][y])
-                        sunflower_times[x][y] = time.time()
-            draw_cursor(pygame.mouse.get_pos())
+            mouse_x, mouse_y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
             pygame.display.flip()
             clock.tick(60)
             pygame.display.set_caption("fps: " + str(clock.get_fps()))
+        open_windows()
+        draw_plants()
+        draw_zombies()
+        draw_sun()
+        draw_cursor((mouse_x, mouse_y))
+        pygame.display.flip()
 pygame.quit()
